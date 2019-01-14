49f5d8720cf93045d6e46fbb27f97fcc3fc83b78
MrceL <9719marcel@gmail.com>
2019-01-14 21:47:36 +0100

    Added file-generation support to StackBlitz Component.

--- src/app/stack-bitz/stack-bitz.component.ts
+++ src/app/stack-bitz/stack-bitz.component.ts
@@ -1,8 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import sdk from '@stackblitz/sdk'
 
-import { project } from './project/project-info';
 import { Router } from '@angular/router';
+
+// Project Imports
+import { project } from './project/project-info';
+import { project2 } from './project/project-info2';
+import { connectionError } from './project/error';
+
+
 
 @Component({
   selector: 'app-stack-bitz',
@@ -10,13 +16,36 @@ import { Router } from '@angular/router';
   styleUrls: ['./stack-bitz.component.scss']
 })
 export class StackBitzComponent implements OnInit {
+  virtualMachine: any; // TODO: Find and set the Type. Possibilities { 'StackBlitzComponent', 'more...' }
 
   constructor(
     public router: Router
   ) { }
 
   ngOnInit() {
-    sdk.embedProject('editor', project)
+    sdk.embedProject('editor', project, {
+      openFile: 'sampleProject.ts'
+    }).then( vm => {
+      this.virtualMachine = vm;
+    })
   }
 
+  logSDK() {
+    if (!this.virtualMachine) {
+      console.error(connectionError)
+      return -1;
+    }
+
+    this.virtualMachine.applyFsDiff({
+      create: {
+        'sampleProject.ts': `// This file was generated in real time using the StackBlitz Virtual Machine.`
+      },
+      destroy: ['']
+    });
+  }
+
+  refresh() {
+    console.log(project);
+    sdk.embedProject('editor', project2);
+  }
 }
