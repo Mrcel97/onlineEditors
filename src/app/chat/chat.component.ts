f4aca200f2388d9531047c883eef8370c92b7325
MrceL <9719marcel@gmail.com>
2019-02-03 22:37:15 +0100

    Added chat component to the project.

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,4 +1,8 @@
+import { HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import * as Stomp from 'stompjs';
+import * as SockJS from 'sockjs-client';
+import * as S from 'jquery';
 
 @Component({
   selector: 'app-chat',
@@ -6,10 +10,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chat.component.scss']
 })
 export class ChatComponent implements OnInit {
+  private serverUrl = 'http://localhost:8080/socket'
+  private title = 'WebSockets chat';
+  private stompClient;
 
-  constructor() { }
+  constructor() {
+    this.initializeWebSocketConnection();
+  }
 
   ngOnInit() {
   }
 
+  initializeWebSocketConnection(){
+    let ws = new SockJS(this.serverUrl);
+    this.stompClient = Stomp.over(ws);
+    let that = this;
+    this.stompClient.connect({}, function(frame) {
+      that.stompClient.subscribe("/chat", (message) => {
+        if(message.body) {
+          $(".chat").append("<div class='message'>"+message.body+"</div>")
+          console.log(message.body);
+        }
+      });
+    });
+  }
+
+  sendMessage(message){
+    this.stompClient.send("/app/send/message" , {}, message);
+    $('#input').val('');
+  }
 }
